14c197f307f143fdbc53c7a04fbb976819f71d6f
Stuart Urback <stuart@urback.net>
2016-03-29 10:12:03 -0600

    Complete exercises

--- nested_objects_arrays.js
+++ nested_objects_arrays.js
@@ -1,10 +1,28 @@
 function curriculum(data) {
+  var newObj = {};
+  data.units.forEach(function(units){
+    newObj[units.name] = units.lessons.map(function(item){
+      return item.name;
+    })
+  })
+  return newObj;
 
 }
 
 function lessonOccurrences(data) {
-
+  var newObj = {};
+  data.units.forEach(function(units){
+    newObj[units.name] = {};
+    units.lessons.forEach(function(detailedInfo){
+      newObj[units.name][detailedInfo.name] = Object.keys(detailedInfo.occurrences)
+    })
+  })
+  return newObj;
 }
+
+
+// occurrences.keys
+// Obj.keys(occurrences)
 
 module.exports = {
   curriculum: curriculum,
