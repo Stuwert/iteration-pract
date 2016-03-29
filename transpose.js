e84bccdb6b4ed08f04ce8d9efc2b5f96afdcb95f
Stuart Urback <stuart@urback.net>
2016-03-29 09:57:40 -0600

    Complete transpose

--- transpose.js
+++ transpose.js
@@ -1,5 +1,15 @@
 // This function should transpose the given array.
 
 module.exports = function (input) {
-
+  return input.map(function(arr, i){
+    var newArr = [];
+    input.forEach(function(item, j){
+      item.forEach(function(subItem, k){
+        if(k === i){
+          newArr.push(subItem);
+        }
+      })
+    })
+    return newArr;
+  })
 };
